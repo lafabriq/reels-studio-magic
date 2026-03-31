df99e3e25b73767baebac39b90b498f1bcc116a0
lafabriq <la22447788@gmail.com>
2026-03-31 05:28:56 +0000

    design: darken logo text

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ const Index = () => {
     <div className="min-h-screen bg-white relative">
       {/* Logo top-left */}
       <header className="absolute top-0 left-0 p-8">
-        <span className="text-xs font-light text-neutral-400 tracking-[0.3em] lowercase select-none">
+        <span className="text-xs font-light text-neutral-700 tracking-[0.3em] lowercase select-none">
           la content fabrique
         </span>
       </header>
